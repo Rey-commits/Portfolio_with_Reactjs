830ddf1226cb16988c111b9343df2d71a240a774
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-04-03 07:27:51 +0800

    home page have been modified

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -6,8 +6,14 @@ function HomePage() {
             <header className="hero">
                 <h1 className="hero-text">
                     Hi, I am 
-                    <span>Lorem Ipsum. To be continue at 46:30</span>
+                    <span>Reymond Mesuga</span>
                 </h1>
+                <p className="h-sub-text">
+                    It is a long established fact that a reader will be 
+                    distracted by the readable content of a page when looking 
+                    at its layout.
+                </p>
+                <div className="icons"></div>
             </header>
         </div>
     )

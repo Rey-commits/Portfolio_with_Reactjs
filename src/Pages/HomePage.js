6f5cb4a3f7adc7775495d7e324aee70f39a7b381
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-04-02 12:31:55 +0800

    main content is now being modified

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 function HomePage() {
     return (
-        <div>
-            <h1>HomePage</h1>
+        <div className="HomePage">
+            <header className="hero">
+                <h1 className="hero-text">
+                    Hi, I am 
+                    <span>Lorem Ipsum.</span>
+                </h1>
+            </header>
         </div>
     )
 }

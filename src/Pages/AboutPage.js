6ffb220b3d488ec5c7bc7040fe76eb5b206653ce
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-04-03 11:19:48 +0800

    about page have been modified

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Title from '../Components/Title'
+import ImageSection from './ImageSection'
 
 function AboutPage() {
     return (
         <div className="AboutPage">
             <Title title={"About Me"} span={"About Me"}/>
+            <ImageSection />
         </div>
     )
 }
 
-export default AboutPage
+export default AboutPage;
--- src/Pages/ImageSection.js
+++ src/Pages/ImageSection.js
@@ -1,21 +1,54 @@
 import React from 'react'
-import about from "../img/about.png"
+import reymond1 from "../img/reymond1.jpg"
 
 function ImageSection() {
     return (
         <div className="ImageSection">
             <div className="img">
-                <img src={about} alt=""/>
+                <img src={reymond1} alt=""/>
             </div>
             <div className="about-info">
-                <h4>I am<span>Reymond Mesuga</span></h4>
+                <h4>I am<span> Reymond Mesuga</span></h4>
                 <p>
                      It is a long established fact that a reader will be 
                     distracted by the readable content of a page when looking 
                     at its layout.
                 </p>
                 <div className="about-details">
-                    
+                    <div className="details">
+                        <p>Full Name</p>
+                        <p>: Reymond Robin Mesuga</p>
+                    </div>
+                </div>
+                <div className="about-details">
+                    <div className="details">
+                        <p>Age</p>
+                        <p>: 21</p>
+                    </div>
+                </div>
+                <div className="about-details">
+                    <div className="details">
+                        <p>Nationality</p>
+                        <p>: Filipino</p>
+                    </div>
+                </div>
+                <div className="about-details">
+                    <div className="details">
+                        <p>Languages</p>
+                        <p>: Filipino and English</p>
+                    </div>
+                </div>
+                <div className="about-details">
+                    <div className="details">
+                        <p>Hometown</p>
+                        <p>: Metro Manila, Philippines</p>
+                    </div>
+                </div>
+                <div className="about-details">
+                    <div className="details">
+                        <p>Full Name</p>
+                        <p>: Reymond Robin Mesuga</p>
+                    </div>
                 </div>
             </div>
         </div>

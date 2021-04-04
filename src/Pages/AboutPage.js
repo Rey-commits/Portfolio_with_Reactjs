74dd0d23ec9c5979a2a45022bd9c3c26a692fa2d
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-04-04 10:00:55 +0800

    skills section is fixed already

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -9,9 +9,24 @@ function AboutPage() {
             <Title title={"ABOUT ME"} span={"ABOUT ME"}/>
             <ImageSection />
             <Title title={"MY SKILLS"} span={"MY SKILLS"}/>
-            <div className="skills-container">
-               <SkillsSection skill={"JavaScript"} progress={"70%"} width={"70%"}/>
-            </div>
+            <div className="container-for-grid">
+                <div className="skillsContainer">
+                    <SkillsSection skill={"JavaScript"} progress={"75%"} width={"75%"}/>
+                    <SkillsSection skill={"React.js"} progress={"80%"} width={"80%"}/>
+                    <SkillsSection skill={"HTML5"} progress={"95%"} width={"95%"}/>
+                    <SkillsSection skill={"CSS3"} progress={"75%"} width={"75%"}/>
+                    <SkillsSection skill={"SASS"} progress={"75%"} width={"75%"}/>
+                    <SkillsSection skill={"Python"} progress={"85%"} width={"85%"}/>
+                    <SkillsSection skill={"Malplotlib"} progress={"90%"} width={"90%"}/>
+                    <SkillsSection skill={"Seaborn"} progress={"80%"} width={"80%"}/>
+                    <SkillsSection skill={"Pandas"} progress={"80%"} width={"80%"}/>
+                    <SkillsSection skill={"Matlab"} progress={"80%"} width={"80%"}/>
+                    <SkillsSection skill={"Scilab"} progress={"83%"} width={"83%"}/>
+                    <SkillsSection skill={"GNU Octave"} progress={"85%"} width={"85%"}/>
+                    <SkillsSection skill={"Linux Kernel"} progress={"78%"} width={"78%"}/>
+                    <SkillsSection skill={"Bash (Unix Shell)"} progress={"85%"} width={"85%"}/>
+                </div> 
+            </div>         
         </div>
     )
 }

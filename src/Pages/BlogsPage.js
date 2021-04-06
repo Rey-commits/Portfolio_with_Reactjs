fe3e48d9538ddabf6aab951cbd4e905c4840b9af
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-04-07 04:27:23 +0800

    blogs page is being added

--- src/Pages/BlogsPage.js
+++ src/Pages/BlogsPage.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import allBlogs from "../Components/allBlogs"
 
 function BlogsPage() {
     return (
-        <div>
-            <h1>Blogs Page</h1>
+        <div className="BlogsPage">
+            {
+                allBlogs.map((blog) => {
+                    return <div className="blog" key={blog.id} >
+                        <div className="blog-content">
+                            <img src={blog.image} alt=""/>
+                            <a href="github.com" className="blog-link">
+                                How To Install and Use Github easily.
+                            </a>
+                        </div>
+                    </div>
+                })
+            }
         </div>
     )
 }

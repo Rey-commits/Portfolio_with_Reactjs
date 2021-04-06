9f65254854d90b6a41a2198cea32059da80e9b65
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-04-07 06:07:40 +0800

    contacts page have been modified

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import ContactItem from '../Components/ContactItem';
+import phone from "../img/phone.svg"
+import email from "../img/emailme.svg"
+import location from "../img/location.svg"
 
 function ContactPage() {
     return (
-        <div>
-            <h1>Contact Page</h1>
+        <div className="ContactPage">
+            <div className="map-section">
+                <ContactItem icon={phone} text1={"+123 34567789"} text2={"+0333 6789 4567"} title={"Phone"}/>
+                <ContactItem icon={email} text1={"+123 34567789"} text2={"+0333 6789 4567"} title={"Email"}/>
+                <ContactItem icon={location} text1={"+123 34567789"} text2={"+0333 6789 4567"} title={"Address"}/>
+            </div>
         </div>
     )
 }

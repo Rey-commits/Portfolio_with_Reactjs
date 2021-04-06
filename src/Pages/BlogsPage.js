44cf0f7be6fd1de60dc15e4cf70b26aca78165c3
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-04-07 05:16:42 +0800

    blogs page is finished

--- src/Pages/BlogsPage.js
+++ src/Pages/BlogsPage.js
@@ -1,24 +1,31 @@
 import React from 'react'
 import allBlogs from "../Components/allBlogs"
+import Title from "../Components/Title"
 
 function BlogsPage() {
     return (
-        <div className="BlogsPage">
-            {
-                allBlogs.map((blog) => {
-                    return <div className="blog" key={blog.id} >
-                        <div className="blog-content">
-                            <img src={blog.image} alt=""/>
-                            <a href={blog.link} className="blog-link">
-                                {blog.title}
-                            </a>
-                            <div className="date-con">
-                                
+        <div>
+            <div className="b-title">
+                <Title title={"RECENT ARTICLES"} span={"RECENT ARTICLES"} />
+            </div>
+            <div className="BlogsPage">
+                {
+                    allBlogs.map((blog) => {
+                        return <div className="blog" key={blog.id} >
+                            <div className="blog-content">
+                                <img src={blog.image} alt=""/>
+                                <a href={blog.link} className="blog-link">
+                                    {blog.title}
+                                </a>
+                                <div className="date-con">
+
+                                </div>
                             </div>
                         </div>
-                    </div>
-                })
-            }
+                    })
+                }
+            </div>
+            
         </div>
     )
 }

e88bb28405b9015f43974dfddebd63bc72539403
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-04-10 11:23:07 +0800

    forfolios page have been modified

--- src/Pages/PortfoliosPage.js
+++ src/Pages/PortfoliosPage.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import Categories from '../Components/Categories'
+import MenuItem from '../Components/MenuItem'
 import Title from '../Components/Title'
 
 function PortfoliosPage() {
     return (
         <div className="PortfolioPage">
             <div className="title">
-                <Title title={"ABOUT ME"} span={"ABOUT ME"}/>
+                <Title title={"Portfolios"} span={"Portfolios"}/>
+            </div>
+            <div className="portfolios">
+                <Categories />
+                <MenuItem />
             </div>
         </div>
     )
